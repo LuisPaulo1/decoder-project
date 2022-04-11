636b7e8367e93baa8254ab4f4163c97c2f89d3fe
LuisPaulo1 <csluispaulo@gmail.com>
2022-04-11 11:37:17 -0300

    semana 1

---
+++ authuser/src/main/java/com/ead/authuser/service/UserService.java
@@ -0,0 +1,23 @@
+package com.ead.authuser.service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
+import com.ead.authuser.models.UserModel;
+
+public interface UserService {
+	
+	List<UserModel> findAll();
+	Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable);
+	Optional<UserModel> findById(UUID id);
+	void delete(UserModel userModel);
+	void save(UserModel userModel);
+	boolean existsByUsername(String username);
+	boolean existsByEmail(String email);
+	
+}
---
+++ authuser/src/main/java/com/ead/authuser/service/impl/UserServiceImpl.java
@@ -0,0 +1,57 @@
+package com.ead.authuser.service.impl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import com.ead.authuser.models.UserModel;
+import com.ead.authuser.repositories.UserRepository;
+import com.ead.authuser.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Override
+	public List<UserModel> findAll() {
+		return userRepository.findAll();
+	}
+		
+	@Override
+	public Page<UserModel> findAll(Specification<UserModel> spec, Pageable pageable) {
+		return userRepository.findAll(spec, pageable);
+	}
+
+	@Override
+	public Optional<UserModel> findById(UUID id) {
+		return userRepository.findById(id);
+	}
+
+	@Override
+	public void delete(UserModel userModel) {
+		userRepository.delete(userModel);		
+	}
+
+	@Override
+	public void save(UserModel userModel) {
+		userRepository.save(userModel);		
+	}
+
+	@Override
+	public boolean existsByUsername(String username) {		
+		return userRepository.existsByUsername(username);
+	}
+
+	@Override
+	public boolean existsByEmail(String email) {		
+		return userRepository.existsByEmail(email);
+	}
+}
