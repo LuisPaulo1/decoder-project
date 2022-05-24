e8fcb48931ac6c72ade5c922c8cd2d2055ac4cc8
LuisPaulo1 <csluispaulo@gmail.com>
2022-05-23 23:16:31 -0300

    Mapear State Transfer Pattern com Replicação Parcial de Dados

--- course/src/main/java/com/ead/course/models/UserModel.java
+++ course/src/main/java/com/ead/course/models/UserModel.java
@@ -1,13 +1,22 @@
 package com.ead.course.models;
 
+import java.io.Serializable;
+import java.util.Set;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
-import java.io.Serializable;
-import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -16,9 +25,32 @@ import java.util.UUID;
 @Entity
 @Table(name = "TB_USERS")
 public class UserModel implements Serializable {
+	
     private static final long serialVersionUID = 1L;
 
     @Id
     private UUID userId;
+    
+    @Column(nullable = false, unique = true, length = 50)
+    private String email;
+    
+    @Column(nullable = false, length = 150)
+    private String fullName;
+    
+    @Column(nullable=false)
+    private String userStatus;
+    
+    @Column(nullable=false)
+    private String userType;
+    
+    @Column(length = 20)
+    private String cpf;
+    
+    @Column
+    private String imageUrl;
+    
+    @ManyToMany(mappedBy = "users",fetch = FetchType.LAZY)
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private Set<CourseModel> courses;
 
 }
