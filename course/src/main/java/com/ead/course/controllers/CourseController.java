1c232fd1d5fae4666864155df13f891b8a7ad35f
LuisPaulo1 <csluispaulo@gmail.com>
2022-07-04 22:43:20 -0300

    Access Token Pattern in Course Microservice

--- course/src/main/java/com/ead/course/controllers/CourseController.java
+++ course/src/main/java/com/ead/course/controllers/CourseController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -47,6 +48,7 @@ public class CourseController {
     @Autowired
     CourseValidator courseValidator;
 
+    @PreAuthorize("hasAnyRole('INSTRUCTOR')")
     @PostMapping
     public ResponseEntity<Object> saveCourse(@RequestBody CourseDto courseDto, Errors errors){
     	log.debug("POST saveCourse courseDto received {} ", courseDto.toString());
@@ -62,7 +64,8 @@ public class CourseController {
         log.info("Course saved successfully courseId {} ", courseModel.getCourseId());
         return ResponseEntity.status(HttpStatus.CREATED).body(courseService.save(courseModel));
     }
-
+    
+    @PreAuthorize("hasAnyRole('INSTRUCTOR')")
     @DeleteMapping("/{courseId}")
     public ResponseEntity<Object> deleteCourse(@PathVariable(value="courseId") UUID courseId){
         log.debug("DELETE deleteCourse courseId received {} ", courseId);
@@ -75,7 +78,8 @@ public class CourseController {
         log.info("Course deleted successfully courseId {} ", courseId);
         return ResponseEntity.status(HttpStatus.OK).body("Course deleted successfully.");
     }
-
+    
+    @PreAuthorize("hasAnyRole('INSTRUCTOR')")
     @PutMapping("/{courseId}")
     public ResponseEntity<Object> updateCourse(@PathVariable(value="courseId") UUID courseId,
                                                @RequestBody @Valid CourseDto courseDto){
@@ -96,7 +100,8 @@ public class CourseController {
         log.info("Course updated successfully courseId {} ", courseModel.getCourseId());
         return ResponseEntity.status(HttpStatus.OK).body(courseModel);
     }
-
+    
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @GetMapping
     public ResponseEntity<Page<CourseModel>> getAllCourses(SpecificationTemplate.CourseSpec spec,
                                                            @PageableDefault(page = 0, size = 10, sort = "courseId", direction = Sort.Direction.ASC) Pageable pageable,
@@ -108,7 +113,8 @@ public class CourseController {
              return ResponseEntity.status(HttpStatus.OK).body(courseService.findAll(spec, pageable));
          }
     }
-
+    
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @GetMapping("/{courseId}")
     public ResponseEntity<Object> getOneCourse(@PathVariable(value="courseId") UUID courseId){
         Optional<CourseModel> courseModelOptional = courseService.findById(courseId);
--- course/src/main/java/com/ead/course/controllers/ModuleController.java
+++ course/src/main/java/com/ead/course/controllers/ModuleController.java
@@ -15,6 +15,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -43,7 +44,8 @@ public class ModuleController {
 
     @Autowired
     CourseService courseService;
-
+    
+    @PreAuthorize("hasAnyRole('INSTRUCTOR')")
     @PostMapping("/courses/{courseId}/modules")
     public ResponseEntity<Object> saveModule(@PathVariable(value="courseId") UUID courseId,
                                                 @RequestBody @Valid ModuleDto moduleDto){
@@ -61,7 +63,8 @@ public class ModuleController {
         log.info("Module saved successfully moduleId {} ", moduleModel.getModuleId());
         return ResponseEntity.status(HttpStatus.CREATED).body(moduleModel);
     }
-
+    
+    @PreAuthorize("hasAnyRole('INSTRUCTOR')")
     @DeleteMapping("/courses/{courseId}/modules/{moduleId}")
     public ResponseEntity<Object> deleteModule(@PathVariable(value="courseId") UUID courseId,
                                                @PathVariable(value="moduleId") UUID moduleId){
@@ -75,7 +78,8 @@ public class ModuleController {
         log.info("Module deleted successfully moduleId {} ", moduleId);
         return ResponseEntity.status(HttpStatus.OK).body("Module deleted successfully.");
     }
-
+    
+    @PreAuthorize("hasAnyRole('INSTRUCTOR')")
     @PutMapping("/courses/{courseId}/modules/{moduleId}")
     public ResponseEntity<Object> updateModule(@PathVariable(value="courseId") UUID courseId,
                                                @PathVariable(value="moduleId") UUID moduleId,
@@ -93,14 +97,16 @@ public class ModuleController {
         log.info("Module updated successfully moduleId {} ", moduleModel.getModuleId());
         return ResponseEntity.status(HttpStatus.OK).body(moduleModel);
     }
-
+    
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @GetMapping("/courses/{courseId}/modules")
     public ResponseEntity<Page<ModuleModel>> getAllModules(@PathVariable(value="courseId") UUID courseId,
                                                            SpecificationTemplate.ModuleSpec spec,
                                                            @PageableDefault(page = 0, size = 10, sort = "moduleId", direction = Sort.Direction.ASC) Pageable pageable){
         return ResponseEntity.status(HttpStatus.OK).body(moduleService.findAllByCourse(SpecificationTemplate.moduleCourseId(courseId).and(spec), pageable));
     }
-
+    
+    @PreAuthorize("hasAnyRole('STUDENT')")
     @GetMapping("/courses/{courseId}/modules/{moduleId}")
     public ResponseEntity<Object> getOneModule(@PathVariable(value="courseId") UUID courseId,
                                                @PathVariable(value="moduleId") UUID moduleId){
